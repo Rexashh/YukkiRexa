1d6219de72bd6c555bd9771d2307b6eea0f5a76d
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-04-14 23:20:57 +0700

    removelang

--- YukkiMusic/plugins/tools/languages.py
+++ YukkiMusic/plugins/tools/languages.py
@@ -29,36 +29,6 @@ def lanuages_keyboard(_):
             callback_data=f"languages:en",
         ),
         InlineKeyboardButton(
-            text="🇮🇳 हिन्दी",
-            callback_data=f"languages:hi",
-        ),
-    )
-    keyboard.row(
-        InlineKeyboardButton(
-            text="🇱🇰 සිංහල",
-            callback_data=f"languages:si",
-        ),
-        InlineKeyboardButton(
-            text="🇦🇿 Azərbaycan",
-            callback_data=f"languages:az",
-        ),
-    )
-    keyboard.row(
-        InlineKeyboardButton(
-            text="🇮🇳 ગુજરાતી",
-            callback_data=f"languages:gu",
-        ),
-        InlineKeyboardButton(
-            text="🇹🇷 Türkiye Türkçesi",
-            callback_data=f"languages:tr",
-        ),
-    )
-    keyboard.row(
-        InlineKeyboardButton(
-            text="🐶 Cheems",
-            callback_data=f"languages:cheems",
-        ),
-        InlineKeyboardButton(
             text="🇮🇩 Indonesia",
             callback_data=f"languages:id",
         ),
