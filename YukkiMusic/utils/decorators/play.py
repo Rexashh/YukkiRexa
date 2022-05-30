6f72961f2200c7aad8626bef98b9315a4c25e33a
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-05-30 16:03:33 +0700

    Update play.py

--- YukkiMusic/utils/decorators/play.py
+++ YukkiMusic/utils/decorators/play.py
@@ -27,7 +27,7 @@ def PlayWrapper(command):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "Bot is under maintenance. Please wait for some time..."
+                    "Bot sedang dalam pemeliharaan. Mohon tunggu beberapa saat..."
                 )
         if PRIVATE_BOT_MODE == str(True):
             if not await is_served_private_chat(message.chat.id):
